8af999329a653c43f0cd82bbb49f407cf58a413a
maxhryhorczuk <133361534+maxhryhorczuk@users.noreply.github.com>
2025-04-15 11:39:52 -0500

    Create 04_insert_sample_data.sql
    I inserted data into the "customers" table using SQL, which helped me understand how to use the INSERT INTO statement.

---
+++ 04_insert_sample_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.customers (name, email)
+VALUES 
+    ('Marry Brown', 'marry@gmail.com'),
+    ('John Smith', 'john.smith@example.com'),
+    ('Jane Doe', 'jane.doe@example.com');
